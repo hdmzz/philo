cfa5511a4826ab8d3a16c68e8767f9d6e5e65e17
hugo <hdamitzi@student.42lyon.fr>
2023-05-17 15:15:14 +0200

    leaks

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,18 +6,27 @@
 /*   By: hdamitzi <hdamitzi@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 16:55:24 by hdamitzi          #+#    #+#             */
-/*   Updated: 2023/05/16 17:07:00 by hdamitzi         ###   ########.fr       */
+/*   Updated: 2023/05/17 15:14:53 by hdamitzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-void	free_all(t_philo *philo)
+void	free_all(t_args *args)
 {
-	//free(philo->args->forks);
-	pthread_mutex_destroy(&philo->check_meal_mutex);
-	pthread_mutex_destroy(&philo->args->print_mutex);
+	int	i;
 
+	i = -1;
+	while (++i < args->nb_philo)
+	{
+		pthread_mutex_destroy(&args->forks[i]);
+		pthread_mutex_destroy(&args->philos[i].check_meal_mutex);
+	}
+	pthread_mutex_destroy(&args->print_mutex);
+	pthread_mutex_destroy(&args->check_death);
+	free(args->forks);
+	free(args->philos);
+	pthread_mutex_destroy(&args->print_mutex);
 }
 
 int	main(int ac, char **av)
@@ -30,6 +39,6 @@ int	main(int ac, char **av)
 	init_philo(&args);
 	create_threads(&args);
 	wait_and_end(&args);
-
+	free_all(&args);
 	return (0);
 }
